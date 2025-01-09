6b85c0d1a72e049161100190b678f6276e0b0254
tsotnegujabidze <tsotnegujabidze3@gmail.com>
2025-01-09 20:37:37 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// when no clicked, make button go to random place
+
+const no = document.getElementById("no");
+
+const button = document.querySelector("button");
+
+no.addEventListener("click", () => {
+    const x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+    const y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+    no.style.position = "absolute";
+    no.style.left = `${x}px`;
+    no.style.top = `${y}px`;
+});
+
